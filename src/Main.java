30e7f28c88b5d3b7dc95e134e90abcd995e883ab
Goodwin999 <Vyacheslav.tsitsar@mail.ru>
2023-09-27 17:34:29 +0300

    Fix task 1/Выполнил первую задачу

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+
+    public static void checksForLeapYear(int leapYear) {
+        System.out.println(leapYear + " год - високосный год");
+    }
+
+    public static void checksForNonleapYears(int nonLeapYear) {
+        System.out.println(nonLeapYear + " год - не високосный год");
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Home work 11");
+
+        System.out.println("Task1");
+        int year = 2021;
+        boolean leapYear = (year > 1584 && ((year % 400 == 0) ||
+                (year % 4 == 0 && year % 100 != 0)));
+        if (leapYear) {
+            checksForLeapYear(year);
+        } else {
+            checksForNonleapYears(year);
+        }
+
+
+
+
+
     }
 }
